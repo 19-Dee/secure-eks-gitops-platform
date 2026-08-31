c6563d1497d6ac7c0246111748b374d818793362
cloudsecdee@gmail.com <19dishen@gmail.com>
2026-08-31 14:26:35 +0100

    Add NAT gateways and associated resources for public subnets; update route table associations and naming conventions

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -6,7 +6,39 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-resource "aws_route_table" "public-route" {
+resource "aws_nat_gateway" "pubsub_a_ngw" {
+  allocation_id = aws_eip.pubsub_a_eip.id
+  subnet_id     = aws_subnet.pubsub_a.id
+
+  tags = {
+    Name = "gw NAT for public subnet a"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_eip" "pubsub_a_eip" {
+}
+
+resource "aws_nat_gateway" "pubsub_b_ngw" {
+  allocation_id = aws_eip.pubsub_b_eip.id
+  subnet_id     = aws_subnet.pubsub_b.id
+
+  tags = {
+    Name = "gw NAT for public subnet b"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_eip" "pubsub_b_eip" {
+}
+
+resource "aws_route_table" "public_route" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -15,16 +47,29 @@ resource "aws_route_table" "public-route" {
   }
 
   tags = {
-    Name = "Public Route"
+    Name = "public-route-table"
   }
 }
 
-resource "aws_route_table_association" "pubsub-a-rta" {
+
+
+
+resource "aws_route_table_association" "pubsub_a_rta" {
   subnet_id      = aws_subnet.pubsub_a.id
-  route_table_id = aws_route_table.public-route.id
+  route_table_id = aws_route_table.public_route.id
 }
 
-resource "aws_route_table_association" "pubsub-b-rta" {
+resource "aws_route_table_association" "pubsub_b_rta" {
   subnet_id      = aws_subnet.pubsub_b.id
-  route_table_id = aws_route_table.public-route.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+resource "aws_nat_gateway_eip_association" "pubsub_a_eip_association" {
+  allocation_id  = aws_eip.pubsub_a_eip.id
+  nat_gateway_id = aws_nat_gateway.pubsub_a_ngw.id
+}
+
+resource "aws_nat_gateway_eip_association" "pubsub_b_eip_association" {
+  allocation_id  = aws_eip.pubsub_b_eip.id
+  nat_gateway_id = aws_nat_gateway.pubsub_b_ngw.id
 }

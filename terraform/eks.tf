0f1d152ce7fd0cdf18beff22629bf20998028a7e
cloudsecdee@gmail.com <19dishen@gmail.com>
2026-08-31 16:31:57 +0100

    Refactor EKS cluster and node group resources: update naming conventions and add IAM role configurations

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,5 +1,5 @@
-resource "aws_eks_cluster" "multienv-eks-cluster" {
-  name = "multienv-eks-cluster"
+resource "aws_eks_cluster" "multienv_eks_cluster" {
+  name = "multienv_eks_cluster"
 
   access_config {
     authentication_mode = "API"
@@ -10,8 +10,6 @@ resource "aws_eks_cluster" "multienv-eks-cluster" {
 
   vpc_config {
     subnet_ids = [
-      aws_subnet.pubsub_a.id,
-      aws_subnet.pubsub_b.id,
       aws_subnet.privsub_a.id,
       aws_subnet.privsub_b.id,
     ]
@@ -25,26 +23,27 @@ resource "aws_eks_cluster" "multienv-eks-cluster" {
   ]
 }
 
-resource "aws_iam_role" "cluster" {
-  name = "multienv-eks-cluster-iamrole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "sts:AssumeRole",
-          "sts:TagSession"
-        ]
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
+resource "aws_eks_node_group" "my_eks_ng" {
+  cluster_name    = aws_eks_cluster.multienv_eks_cluster.name
+  node_group_name = "my_eks_ng"
+  node_role_arn   = aws_iam_role.multienv_eks_cluster_iam_role.arn
+  subnet_ids      = [aws_subnet.privsub_a.id, aws_subnet.privsub_b.id]
 
-resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster.name
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.multienv_eks_cluster_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.multienv_eks_cluster_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.multienv_eks_cluster_AmazonEC2ContainerRegistryReadOnly,
+  ]
 }

102177a1792d34e6153df5968c0977e940673ee1
cloudsecdee@gmail.com <19dishen@gmail.com>
2026-08-31 16:50:29 +0100

    Fix EKS node group role ARN reference and update IAM role policy attachments

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -26,7 +26,7 @@ resource "aws_eks_cluster" "multienv_eks_master" {
 resource "aws_eks_node_group" "my_eks_ng" {
   cluster_name    = aws_eks_cluster.multienv_eks_master.name
   node_group_name = "my_eks_ng"
-  node_role_arn   = aws_iam_role.multienv_eks_master_iam_role.arn
+  node_role_arn   = aws_iam_role.multienv_eks_worker_node_iam_role.arn
   subnet_ids      = [aws_subnet.privsub_a.id, aws_subnet.privsub_b.id]
 
   scaling_config {
@@ -42,9 +42,9 @@ resource "aws_eks_node_group" "my_eks_ng" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.multienv_eks_master_AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.multienv_eks_master_AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.multienv_eks_master_AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.multienv_eks_worker_node_iam_role_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.multienv_eks_worker_node_iam_role_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.multienv_eks_worker_node_iam_role_AmazonEC2ContainerRegistryReadOnly,
   ]
 }
 

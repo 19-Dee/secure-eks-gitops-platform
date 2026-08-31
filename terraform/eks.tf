279fa99c677eb87ad194c51933c6091f373ab1ec
cloudsecdee@gmail.com <19dishen@gmail.com>
2026-08-31 16:36:52 +0100

    Add IAM role for EKS cluster with assume role policy and attach AmazonEKSClusterPolicy

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -47,3 +47,27 @@ resource "aws_eks_node_group" "my_eks_ng" {
     aws_iam_role_policy_attachment.multienv_eks_cluster_AmazonEC2ContainerRegistryReadOnly,
   ]
 }
+
+resource "aws_iam_role" "multienv_eks_cluster_iam_role" {
+  name = "multienv_eks_cluster_iam_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.multienv_eks_cluster_iam_role.name
+}

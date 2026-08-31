a51c64bb06991f90caf615e0666590074f0ee270
cloudsecdee@gmail.com <19dishen@gmail.com>
2026-08-31 16:53:35 +0100

    Fix IAM role references for EKS worker node policy attachments

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -15,15 +15,15 @@ resource "aws_iam_role" "multienv_eks_worker_node_iam_role" {
 
 resource "aws_iam_role_policy_attachment" "multienv_eks_master_AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.multienv_eks_master_iam_role.name
+  role       = aws_iam_role.multienv_eks_worker_node_iam_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "multienv_eks_master_AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.multienv_eks_master_iam_role.name
+  role       = aws_iam_role.multienv_eks_worker_node_iam_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "multienv_eks_master_AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.multienv_eks_master_iam_role.name
+  role       = aws_iam_role.multienv_eks_worker_node_iam_role.name
 }
